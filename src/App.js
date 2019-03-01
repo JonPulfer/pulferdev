31ff70c1f58e9f97f3279dae627d3b2f08d46248
Jonathan Pulfer <jonathan.pulfer@entura.co.uk>
2019-03-01 13:33:47 +0000

    Tidy up nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Form from 'react-bootstrap/Form';
 import './App.css';
 
 class App extends Component {
@@ -18,19 +16,15 @@ class App extends Component {
               <Nav.Link href="#travel">Travel Info</Nav.Link>
               <Nav.Link href="#about">About</Nav.Link>
             </Nav>
-            <Form inline>
-              <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-success">Search</Button>
-            </Form>
+            <Nav>
+              <Nav.Link href="https://twitter.com/jonathanpulfer">@jonathanpulfer</Nav.Link>
+            </Nav>
           </Navbar.Collapse>
         </Navbar>
         <Jumbotron>
           <h1>Hello, world!</h1>
           <p>
             Here be dragons...
-          </p>
-          <p>
-            <Button variant="primary">Hunt those dragons</Button>
           </p>
         </Jumbotron>
       </div>
