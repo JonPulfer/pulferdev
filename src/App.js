1fdf7029fa65ec8d43ed2825754e7ecc79735b6c
Jonathan Pulfer <jonathan.pulfer@entura.co.uk>
2019-03-01 09:04:16 +0000

    First few forays into the react-bootstrap world.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Jonathan Pulfer</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#travel">Travel Info</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+            </Nav>
+            <Form inline>
+              <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+        <Jumbotron>
+          <h1>Hello, world!</h1>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Here be dragons...
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <p>
+            <Button variant="primary">Hunt those dragons</Button>
+          </p>
+        </Jumbotron>
       </div>
     );
   }
